1f51992fc70ddc6a100146a2b5553c4cec5c9a52
eliagbayani <eli@eol.org>
2018-05-29 10:13:12 -0400

    bulk upload

---
+++ maps/leaf-demo_v2.js
@@ -0,0 +1,103 @@
+// See post: http://asmaloney.com/2015/06/code/clustering-markers-on-leaflet-maps
+
+var map = L.map( 'map', {
+  center: [10.0, 5.0],
+  minZoom: 1,
+  zoom: 2,
+  fullscreenControl: true
+});
+
+// var map_x = L.map('toggle-map', {scrollWheelZoom: false}).setView([37.8, -96], 4); //just other syntax with other attributes
+
+
+L.tileLayer( 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+ attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+ subdomains: ['a','b','c']
+}).addTo( map );
+
+
+L.easyButton( '<strong>Off</strong>', function(){
+  disable();
+}).addTo(map);
+L.easyButton( '<strong>On</strong>', function(){
+  enable();
+}).addTo(map);
+
+// L.easyButton('icon fa-star', function(){alert('button works')}).addTo(map);
+
+/*
+var toggle = L.easyButton({
+  states: [{'<strong>Off</strong>',
+    stateName: 'add-markers',
+    // icon: 'fa-map-marker',
+    title: 'Cluster Off',
+    onClick: function() {
+        disable();
+        control.state('remove-markers');
+    }
+  }, {'<strong>On</strong>',
+    // icon: 'fa-undo',
+    stateName: 'remove-markers',
+    title: 'Cluster On',
+    onClick: function() {
+        enable();
+        control.state('add-markers');
+    }
+  }]
+});
+toggle.addTo(map);
+*/
+
+//----------------------------------------------------------------------------------------------------------------
+//for NavBar
+L.control.navbar().addTo(map);
+//----------------------------------------------------------------------------------------------------------------
+
+var myURL = jQuery( 'script[src$="leaf-demo_v2.js"]' ).attr( 'src' ).replace( 'leaf-demo_v2.js', '' );
+
+var myIcon = L.icon({
+  iconUrl: myURL + 'images/pin24.png',
+  iconRetinaUrl: myURL + 'images/pin48.png',
+  iconSize: [29, 24],
+  iconAnchor: [9, 21],
+  popupAnchor: [0, -14]
+});
+
+// var markerClusters = L.markerClusterGroup();
+var markerClusters = L.markerClusterGroup({
+    // spiderfyOnMaxZoom: false,
+    // showCoverageOnHover: false,
+    // zoomToBoundsOnClick: false
+    maxClusterRadius: 50
+});
+
+var markers = data.records;
+// alert(markers.length);
+for ( var i = 0; i < markers.length; ++i )
+{
+  var popup = markers[i].name +
+              '<br/>' + markers[i].a +
+              '<br/><b>IATA/FAA:</b> ' + markers[i].b +
+              '<br/><b>ICAO:</b> ' + markers[i].c +
+              '<br/><b>Timezone:</b> ' + markers[i].d;
+
+  var m = L.marker( [markers[i].h, markers[i].i], {icon: myIcon} )
+                  .bindPopup( popup );
+
+  markerClusters.addLayer( m );
+}
+
+map.addLayer( markerClusters );
+
+//----------------------------------------------------------------------------------------------------------------
+//for enable disable cluster
+function enable() {
+    alert(markerClusters.freezeAtZoom('max'));
+    markerClusters.enableClustering();
+}
+function disable() {
+    alert(markerClusters.freezeAtZoom('max'));
+    markerClusters.disableClustering();
+}
+//----------------------------------------------------------------------------------------------------------------
+
---
+++ readmeli.txt
@@ -0,0 +1,13 @@
+this was taken from: 
+
+https://asmaloney.com/2015/06/code/clustering-markers-on-leaflet-maps/
+
+downloaded from github: Leaflet_Cluster_Example-master.zip
+https://github.com/asmaloney/Leaflet_Cluster_Example
+
+--------------------------------------------
+
+and prototyping for EOL maps, I've added forlders
+
+/data/
+/vendor/
