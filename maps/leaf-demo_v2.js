9f3adb01b97b598333e3940e61a1be7220f94daf
eliagbayani <eli@eol.org>
2018-05-30 04:50:28 -0400

    count no. of markers while in cluster mode

--- maps/leaf-demo_v2.js
+++ maps/leaf-demo_v2.js
@@ -1,7 +1,12 @@
 // See post: http://asmaloney.com/2015/06/code/clustering-markers-on-leaflet-maps
+// if we want to show left pan or the like: https://github.com/stefanocudini/leaflet-list-markers
+
+latlng = get_all_latlongs(data.records);
+center_map = getCentroid(latlng);
 
 var map = L.map( 'map', {
-  center: [10.0, 5.0],
+  center: center_map,
+  // center: [10.0, 5.0],
   minZoom: 1,
   zoom: 2,
   fullscreenControl: true,
@@ -27,11 +32,8 @@ L.easyButton('<span class="star">&starf;</span>', function(){alert('button works
 */
 
 
-
-
 //----------------------------------------------------------------------------------------------------------------//for NavBar
 L.control.navbar().addTo(map);
-
 //----------------------------------------------------------------------------------------------------------------
 
 var myURL = jQuery( 'script[src$="leaf-demo_v2.js"]' ).attr( 'src' ).replace( 'leaf-demo_v2.js', '' );
@@ -56,8 +58,6 @@ var markerClusters = L.markerClusterGroup({
         return L.divIcon({ html: '<b>' + cluster.getChildCount() + '</b>' });
     }
     */
-    
-    
 });
 
 
@@ -82,14 +82,15 @@ for ( var i = 0; i < markers.length; ++i )
 map.addLayer( markerClusters );
 
 
-//------------------ to center map
-
-// alert("eli");
+//---------------------------------------------------------------------------------------------------------------- to center map 
+/* working but moved above
 var center = getCentroid(latlng);
-alert(center);
-
-map.setView({lat:center[0], lng:center[1]}, 2);
-
+map.setView(center, 2); //can pass array e.g. var center to setView()
+*/
+/* other options: https://stackoverflow.com/questions/12735303/how-to-change-the-map-center-in-leaflet
+map.setView({lat:center[0], lng:center[1]}, 2); //or can pass object to setView()
+map.panTo({lat:center[0], lng:center[1]});
+*/
 //----------------------------------------------------------------------------------------------------------------//for Cluster On/Off
 // /*
 var toggle = L.easyButton({
@@ -97,7 +98,7 @@ var toggle = L.easyButton({
   states: [{
     stateName: 'add-markers',
     icon: 'fa-map-marker',
-    // icon: '<span class="diamond">&diamond;</span>',
+    // icon: '<span class="star">&diamond;</span>',
     title: 'Cluster On',
     onClick: function(control) {
         disable();
@@ -114,7 +115,7 @@ var toggle = L.easyButton({
     }
   }]
 });
-if(markers.length < 20000) {
+if(markers.length < 15000) {
     toggle.addTo(map);
 }
 // */
@@ -122,18 +123,66 @@ if(markers.length < 20000) {
 //----------------------------------------------------------------------------------------------------------------
 //for enable disable cluster
 function enable() {
+    alert("view port: "+getFeaturesInView(map));
     markerClusters.enableClustering();
 }
 function disable() {
     alert(markers.length);
+    alert("view port: "+getFeaturesInView(map));
     markerClusters.disableClustering();
 }
-//----------------------------------------------------------------------------------------------------------------
+function getFeaturesInView(map) {
+  var features = [];
+  var i = 0;
+  map.eachLayer( function(layer) {
+    if(layer instanceof L.Marker) {
+      if(map.getBounds().contains(layer.getLatLng())) {
+        features.push(layer.feature);
+        // alert(console.debug(layer));
+
+        if(isNaN(layer._childCount)){i = i + 1;}
+        else {i = i + layer._childCount;}
+            
+
+        // alert("childCount = " + layer._childCount);
+
+        //__parent: Object { _zoom: 6, _childCount: 8, _iconNeedsUpdate: true, … }
+        // alert(layer['_markers']);
+        // alert(JSON.stringify(layer.feature));
+        // alert(layer.feature);
+        // alert(layer.getLatLng().length);
+      }
+    }
+  });
+
+  alert(i);
+  
+  
+  // var obj = features;
+  // var out = '';
+  // for (var i in obj) {
+  //     out += i + ": " + obj[i] + "\n";
+  // }
+  // alert(out);
+  
+  
+  return features.length;
+}
+//---------------------------------------------------------------------------------------------------------------- from: http://webdevzoom.com/get-center-of-polygon-triangle-and-area-using-javascript-and-php/
 // var getCentroid = function (coord) 
 function getCentroid(coord)
 {
-	var center = coord.reduce(function (x,y) {
-		return [x[0] + y[0]/coord.length, x[1] + y[1]/coord.length] 
-	}, [0,0])
-	return center;
+    var center = coord.reduce(function (x,y) {
+        return [x[0] + y[0]/coord.length, x[1] + y[1]/coord.length] 
+    }, [0,0])
+    return center;
+}
+//----------------------------------------------------------------------------------------------------------------
+function get_all_latlongs(markers)
+{
+    var latlng = [];
+    for ( var i = 0; i < markers.length; ++i ) {
+      latlng.push([markers[i].h, markers[i].i]); //add value to array latlng
+    }
+    return latlng;
 }
